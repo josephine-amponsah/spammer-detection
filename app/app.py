35a2eb82987d9b0c52d50d657d389112d8911620
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-11-04 11:10:36 +0000

    app layout 2, font styling

--- app/app.py
+++ app/app.py
@@ -7,16 +7,27 @@ from dash.dependencies import Input, Output
 
 
 # from app.pages import sales
-
-app = dash.Dash(__name__,  external_stylesheets=[
+FA = "https://fonts.googleapis.com/css?family=Lobster"
+app = dash.Dash(__name__,  external_stylesheets=[ FA,
                 dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 # app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
 app.layout = html.Div([
     html.Div([
         dbc.Container([
-            dbc.Card([
-                "FAKE ACCOUNT POLICE"
+            html.Div([
+                html.H1("FAKE ACCOUNT POLICE", className = "app-title-text"),
+                html.H5("ASSESS CREDIBILITY OF IG ACCOUNTS", className = "app-title-sub"),
+                dbc.Row([
+                    dbc.Col([ 
+                        html.Div("ACCOUNT INFO"),
+                        dbc.Row(),
+                        dbc.Row(),
+                        dbc.Row(),
+                        dbc.Row(),
+                        ]),
+                    dbc.Col("Result")
+                ], className = "active-space")
             ], className = "app-card")
         ], className= "app-container")
     ],className= "main-space" )
