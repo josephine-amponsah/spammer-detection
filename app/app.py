d050fe6fc98e611096e0f4383c290b86b2fa948c
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-11-06 01:30:15 +0000

    input feature layout

--- app/app.py
+++ app/app.py
@@ -16,23 +16,75 @@ app.layout = html.Div([
     html.Div([
         dbc.Container([
             html.Div([
-                html.H1("FAKE ACCOUNT POLICE", className = "app-title-text"),
-                html.H5("ASSESS CREDIBILITY OF IG ACCOUNTS", className = "app-title-sub"),
+                html.H1(["FAKE ACCOUNT POLICE"], className = "app-title-text"),
+                html.H5(["ASSESS CREDIBILITY OF IG ACCOUNTS"], className = "app-title-sub"),
                 dbc.Row([
-                    dbc.Col([ 
-                        html.Div("ACCOUNT INFO"),
-                        dbc.Row(),
-                        dbc.Row(),
-                        dbc.Row(),
-                        dbc.Row(),
-                        ]),
-                    dbc.Col("Result")
+                    dbc.Col([
+                        html.Div(["ACCOUNT INFO"]),
+                        dbc.Row([
+                            dbc.Col([
+                                html.Div(["Username"]),
+                                dcc.Input(value = "")
+                                ]),
+                            dbc.Col([
+                                html.Div(["Full name"]),
+                                dcc.Input(value = "")
+                                ]),
+                    ]),
+                        html.Br(),
+                        dbc.Row([
+                            html.Div(["Bio"]),
+                            dcc.Input(value = "")
+                    ], justify = "center" ),
+                        html.Br(),
+                        dbc.Row([
+                            dbc.Col([
+                                html.Div(["Has Profile Picture?"]),
+                                dcc.Input(value = "")
+                            ]),
+                            dbc.Col([
+                                html.Div(["Link in bio?"]),
+                                dcc.Input(value = "")
+                            ]),
+                            dbc.Col([
+                                html.Div(["Account private?"]),
+                                dcc.Input(value = "")
+                            ]),
+                    ]),
+                        html.Br(),
+                        dbc.Row([
+                            dbc.Col([
+                                html.Div(["Followers"]),
+                                dcc.Input(value = "")
+                            ]),
+                            dbc.Col([
+                                html.Div(["Following"]),
+                                dcc.Input(value = "")
+                            ]),
+                            dbc.Col([
+                                html.Div(["No. of posts"]),
+                                dcc.Input(value = "")
+                            ]),
+                    ]),
+                        ], width=8, className = "input-section"),
+                    dbc.Col([
+                        html.Img([
+                        ], src = "../app/assets/search-img.png")
+                        ] , width =4)
                 ], className = "active-space")
             ], className = "app-card")
         ], className= "app-container")
     ],className= "main-space" )
 ]
 )
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
