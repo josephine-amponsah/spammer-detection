cab65924a58985c63c44b8a4889db7898a0add7e
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2023-01-06 23:22:38 +0000

    resolved callback errors

--- app/app.py
+++ app/app.py
@@ -69,19 +69,18 @@ app.layout = html.Div([
                                 html.Br(),
                                 dbc.Row([
                                     dbc.Col([html.Div(["Following"])], width=4),
-                                    dbc.Col([dcc.Input(id = "following-count", type = "text", placeholder = "no. of follows", className="input-long-box")], width = 7)
+                                    dbc.Col([dcc.Input(id = "following-count", type = "number", placeholder = "no. of follows", className="input-long-box")], width = 7)
                                 ]),
                                 html.Br(),
                                 dbc.Row([
                                     dbc.Col([html.Div(["No. of posts"])], width = 4),
-                                    dbc.Col([dcc.Input(id = "posts-count", type = "text", placeholder = "no. of posts", className="input-long-box")], width = 7)
-                                   
+                                    dbc.Col([dcc.Input(id = "posts-count", type = "number", placeholder = "no. of posts", className="input-long-box")], width = 7)
                             ]),
                                 html.Br(),
                                 dbc.Row([
                                     dbc.Col([
                                         dbc.Button(
-                                            "Investigate", id="model-trigger", className="trigger-button", n_clicks=0
+                                            "Investigate", id="trigger", className="trigger-button", n_clicks=0
                                         )
                                     ], width= 4)
                                 ], justify='center')
@@ -90,7 +89,7 @@ app.layout = html.Div([
                         dbc.Col([
                             dbc.Card([
                                 dbc.Row([
-                                    dbc.Row([html.H2("RESULTS...")],),
+                                    dbc.Row([html.H2(["RESULTS..."], id ="results-text")],),
                                     dbc.Row([html.Img(src='../assets/Search-rafiki.png')])
                                 ],className="image-search")
                             ], className="image-card")
@@ -104,57 +103,77 @@ app.layout = html.Div([
 )
 
 
-# @app.callback(
-#     Output("change-picture", "children"), 
-#     [Input("model-trigger", "n-clicks")],
-#     [State("enter-username", "value")],
-#     [State("enter-display-name", "value")],
-#     [State("enter-bio", "value")],
-#     [State("profile-pic", "value")],
-#     [State("account-privacy", "value")],
-#     [State("url-bio", "value")],
-#     [State("follower-count", "value")],
-#     [State("follow-count", "value")],
-#     [State("post-count", "value")]
-# )
-# def run_assessment(username, fullname, bio, pic, privacy, url, followers, following, posts):
-#     # sourcery skip: inline-immediately-returned-variable
-#     if pic == "Yes":
-#         profile = 0 + 1
-#     if url == "Yes":
-#         url_bio  = 0 + 1
-#     if privacy == "Yes":
-#         account_privacy = 0 + 1
-#     if username.lower() == fullname.lower():
-#         user_is_full = 0 + 1
-#     num_in_user = len(re.findall(r'[0-9]+', username))
-#     fraction_num_user = num_in_user/len(username)
-#     fullname_words = len(fullname.split())
-#     num_in_full = len(re.findall(r'[0-9]+', fullname))
-#     fraction_num_full = num_in_full/len(fullname)
-#     bio_length = len(bio)
-#     follower_count = followers.astype(int)
-#     following_count = following.astype(int)
-#     post_count = posts.astype(int)
-#     data_dict = {
-#     "profile_pic": profile, 
-#     "num_per_length_username": num_in_user,
-#     "fullname_words": fullname_words,
-#     "num_per_length_fullname": num_in_full,
-#     "fullname_is_username": user_is_full,
-#     "bio_length": bio_length,
-#     "url_in_bio": url_bio,
-#     "private":account_privacy,
-#     "#posts":post_count,
-#     "#followers":follower_count,
-#     "#follows":following_count,
+@app.callback(
+    Output("results-text", "children"), 
+    Input("trigger", "n_clicks"),
+    [State("enter-username", "value")],
+    [State("enter-display-name", "value")],
+    [State("enter-bio", "value")],
+    [State("profile-pic", "value")],
+    [State("account-privacy", "value")],
+    [State("url-bio", "value")],
+    [State("follower-count", "value")],
+    [State("following-count", "value")],
+    [State("posts-count", "value")]
+)
+def run_assessment(n, username, fullname, bio, pic, privacy, url, followers, following, posts):
+    # sourcery skip: inline-immediately-returned-variable, lift-return-into-if
+    user_is_full = 0
+    url_bio =0
+    profile = 0
+    account_privacy=0
+    if n == 0:
+        return "Results..."
+    if pic == "Yes":
+        profile = 0 + 1
+    if url == "Yes":
+        url_bio  = 0 + 1
+    if privacy == "Yes":
+        account_privacy = 0 + 1
+    if username.lower() == fullname.lower():
+        user_is_full = 0 + 1
+    #number in username
+    num_in_user = len(re.findall(r'[0-9]+', username))
+    #fraction of username that are numbers
+    fraction_num_user = num_in_user/len(username)
+    #number of words in full name
+    fullname_words = len(fullname.split())
+    #count of digits in in full name
+    num_in_full = len(re.findall(r'[0-9]+', fullname))
+    #fraction of full name that are numbers
+    fraction_num_full = num_in_full/len(fullname)
+    #length of bio (by words)
+    bio_length = len(bio)
+    #number of followers
+    follower_count = followers
+    #number of people they follow
+    following_count = following
+    #number of posts
+    post_count = posts
     
-# }
-#     df = pd.DataFrame(data_dict)
-#     scaler = StandardScaler()
-#     scaled_data =scaler.fit_transform(df)
-#     prediction = model.predict(scaled_data)
-#     return prediction
+    data_dict = {
+        "profile_pic": profile, 
+        "num_per_length_username": fraction_num_user,
+        "fullname_words": fullname_words,
+        "num_per_length_fullname": fraction_num_full,
+        "fullname_is_username": user_is_full,
+        "bio_length": bio_length,
+        "url_in_bio": url_bio,
+        "private":account_privacy,
+        "#posts":post_count,
+        "#followers":follower_count,
+        "#follows":following_count,
+
+    }
+    df = pd.DataFrame(data_dict,  index=[0])
+    scaler = StandardScaler()
+    scaled_data =scaler.fit_transform(df)
+    prediction = model.predict(scaled_data)
+    if prediction == 1:
+        ans = "SPAMMER ACCOUNT"
+    else:
+        ans = "GENUINE ACCOUNT"
+    return ans
 
 
 
