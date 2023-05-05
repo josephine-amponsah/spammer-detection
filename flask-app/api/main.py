317d31ee4dff4374efa6b32f72b6425a265668f9
josephine amponsah <ladyjossy77>
2023-05-05 10:44:00 +0000

    integrated first API endpoint to retrieve user profile

--- flask-app/api/main.py
+++ flask-app/api/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, Form
 from modules.processors import retrieve_data, collect_data
 import requests
 import tasks
@@ -7,25 +7,36 @@ from tasks import process_classify_data, celery_app
 from modules.dataModels import DataSet, OutputData, ProcessedData
 from fastapi . encoders import jsonable_encoder
 from apify_client import ApifyClient
-
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 
-@app.get('/user/{username}')
-async def retrieve_user(username: str):
+@app.get("/")
+def form(request: Request):
+    return templates.TemplateResponse("detector.html", {"request": request})
+
+
+@app.post('/user', response_class=HTMLResponse)
+async def retrieve_user(request: Request, username: str = Form(...)):
     # sourcery skip: inline-immediately-returned-variable, remove-unnecessary-else, swap-if-else-branches
     profile = collect_data(username)
-    if profile["fullName"] == "Restricted profile":
-        return {"error": "Restricted Profile"}
+    if profile[0]["fullName"] == "Restricted profile":
+        return templates.TemplateResponse('detector.html', {'request': request, "message": "Restricted Profile"})
     else:
         result = process_classify_data.delay(profile)
-        return {'task_id': tasks.id, 'message': 'Profile data found.'}
+        return templates.TemplateResponse('detector.html', {'request': request, 'task_id': result.task_id, 'message': 'Profile data found.'})
 
 
-@app.get('user/state')
-async def classify_user(taskID: str):
-    result = celery_app.AsyncResult(taskID)
+@app.get('/user/{task_id}')
+async def classify_user(task_id: str):
+    result = celery_app.AsyncResult(task_id, app=celery_app)
     if result.ready():
         return {'result': result.result}
     else:
